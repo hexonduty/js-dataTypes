07cebbf91479844a6091fa22865fa22565d76df4
Efecan Açıkgöz <132709251+hexonduty@users.noreply.github.com>
2024-02-20 14:31:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+--------------------Data Types------------------
+
+strings ,numbers,booleans,undefined, and null.
+object , function
+
+*/
+
+// let a = 5;
+// let b = Number("10");
+// console.log(a+b)
+
+// let a = 8;
+// let b = parseInt("12");
+// console.log(a+b)
+
+
+// let a = 8;
+// let b = parseFloat("12.23");
+// console.log(a+b)
+
+// CONVERT TO NUMBER FROM STRİNG
+
+let x = 55;
+console.log(typeof x)
+console.log(x)
+
+
+let xx = String(55);
+console.log(typeof xx)
+console.log(xx)
+
+let sonuc = String(true)
+console.log(typeof sonuc)
+console.log(sonuc)
+
+let rakamlar = String([1,2,3,4]);
+console.log(typeof rakamlar)
+console.log(rakamlar)
